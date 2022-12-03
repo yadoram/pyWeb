c48e7c981aee4ac830fc0edb6eeb2c4f209e391c
yadoram <118780923+yadoram@users.noreply.github.com>
2022-12-04 01:52:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('2022년도 지하철 월별 하차 인원')
+st.title('2022년도 지하철 월별 하차 인원 🛤️')
 
 df = pd.read_csv('monthly_subway_statistics_in_seoul.csv')
 df.set_index = df['연번']
@@ -13,7 +13,7 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(df)
 
-st.subheader('역별 하차 인원')
+st.subheader('🚅 역별 하차 인원')
 option = st.selectbox(
     '역 선택', 
     (df['역명']))
@@ -24,7 +24,7 @@ df_grouped = df_grouped.reset_index()
 st.bar_chart(df_grouped, x='수송연월')
 
 
-st.subheader('호선별 하차 인원')
+st.subheader('🚅 호선별 하차 인원')
 df_line = df.groupby('호선').sum()
 lines = df_line.index.tolist()
 option = st.selectbox(
