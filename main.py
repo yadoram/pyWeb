ba286990c08bb48c609f2f9b0d3280dfd2a4d705
yadoram <118780923+yadoram@users.noreply.github.com>
2022-12-05 18:40:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np 
+ 
+import matplotlib.pyplot as plt
 
 st.title('2022년도 지하철 월별 하차 인원 🛤️')
 
@@ -37,3 +40,34 @@ df_grouped = df_grouped.reset_index()
 st.area_chart(df_grouped, x='수송연월')
 
 
+# pd.read_csv를 통하여 승하차 인원 정보 데이터를 데이터프레임 형태로 읽어옵니다.
+metro_all = pd.read_csv("서울시 지하철 호선별 역별 시간대별 승하차 인원 정보.csv", encoding = 'cp949')
+
+
+# metro_all DataFrame 사용월 데이터 확인
+sorted(list(set(metro_all['사용월'])))
+# metro_all DataFrame 호선명 데이터 확인
+sorted(list(set(metro_all['호선명'])))
+# DataFrame 지하철역 데이터 확인
+sorted(list(set(metro_all['지하철역'])))
+# DataFrame 지하철역 데이터 개수 확인
+len(list(set(metro_all['지하철역'])))
+
+st.subheader('🚅 선택한 월의 총 승객수 🚶‍♂️')
+metro_all_line = metro_all.groupby('사용월').sum()
+lines = metro_all_line.index.tolist()
+option = st.selectbox(
+    '사용월 선택', 
+    (lines))
+
+# 선택한 월의 총 승객수만 추출
+metro_recent = metro_all[metro_all['사용월']==option]
+
+# 불필요한 작업일자 컬럼 제거
+metro_recent = metro_recent.drop(columns={'작업일자'})
+
+metro_line = metro_recent.groupby(['호선명']).mean().reset_index()
+metro_line = metro_line.drop(columns='사용월').set_index('호선명')
+metro_line = metro_line.mean(axis=1).sort_values(ascending=False)
+
+st.bar_chart(metro_line)
